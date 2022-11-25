a53714758d021652325d7a2b691702940ea0217c
Ghaith <m.g.alseirawan@gmail.com>
2022-11-25 10:54:30 +0100

    Add files via upload

---
+++ database/src/app/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import datetime
+
+app = FastAPI(title= "add to database")
+
+# End point for saving files to a database to update the model
+@app.post("/upload_image")
+def upload_image(file : UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        suffix = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        with open(file.filename +'_' + suffix, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message" : "There was an error uploading the file"}
+    finally:
+        file.file.close()
