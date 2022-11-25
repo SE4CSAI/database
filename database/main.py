7a69a5a640168a34da78c914ae92605d9495c940
Udesh Habaraduwa <udesh86@gmail.com>
2022-11-25 16:24:39 +0100

    removes uncessary include statments

--- database/main.py
+++ database/main.py
@@ -1,9 +1,4 @@
-import pickle
 import uvicorn
-from fastai.vision.all import *
-from fastai.text.all import *
-from fastai.collab import *
-from fastai.tabular.all import *
 # Functions required for the classification model. These were used during training so they are required when loading the saved model.
 
 if __name__ == "__main__":
